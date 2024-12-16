e9a323deaa2f4567f1b3c8b9bc5d45b0eafb73e9
Your Name <zohrsadeghi@gmail.com>
2024-12-16 13:39:02 +0100

    restarted application with get endpoint

--- obj/Debug/net9.0/DagnysBageriApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/DagnysBageriApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a2291f7cdbfd69fcf723f0e0a91654821b03077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c22e29b76271f091323909e887b5f19b7726eb52")]
 [assembly: System.Reflection.AssemblyProductAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
