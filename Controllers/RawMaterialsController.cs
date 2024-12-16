c22e29b76271f091323909e887b5f19b7726eb52
Your Name <zohrsadeghi@gmail.com>
2024-12-16 13:38:06 +0100

    made rawmaterial get endpoint

--- Controllers/RawMaterialsController.cs
+++ Controllers/RawMaterialsController.cs
@@ -1,9 +1,22 @@
 
 
+using DagnysBageriApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace DagnysBageriApi.Controllers
 {
-    public class RawMaterialsController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RawMaterialsController(DataContext context) : ControllerBase
     {
-        
+        private readonly DataContext _context = context;
+
+        [HttpGet()]
+        public async Task<ActionResult> ListAllMaterials()
+        {
+            var materials = await _context.RawMaterials.ToListAsync();
+            return Ok(new { success = true, data = materials});
+        }
     }
 }
