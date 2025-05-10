88865b905c50a8e244ddfae2e200da0d92ef3e46
Your Name <zohrsadeghi@gmail.com>
2025-05-11 01:48:03 +0200

    Running after many months

--- obj/Debug/net8.0/DagnysBageriApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DagnysBageriApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5d517b6d3fee1bfc86c9686687bbc5802b41c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fd4c3bef0b8e00077e0d04fa87dead192984b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DagnysBageriApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
